cf94127bbd32b7187a60032d12fc6da9ef073d0c
Dolphinman980 <130254302+Dolphinman980@users.noreply.github.com>
2024-11-27 19:11:32 +1100

    nade goes poof and water doesn't crash on the right

--- sandManager.py
+++ sandManager.py
@@ -18,7 +18,7 @@ class tile:
 class tileManager:
     def __init__(self, dimensions: tuple, tileTypes: Dict[int, tile]):
         self.tiles = [[0 for x in range(-1, dimensions[0] + 1)] for y in range(-1, dimensions[1] + 1)]
-        self.updatedTiles = [[0 for x in range(dimensions[0])] for y in range(dimensions[1])]
+        self.updatedTiles = [[0 for x in range(-1, dimensions[0] + 1)] for y in range(-1, dimensions[1] + 1)]
         self.dimensions = dimensions
         self.tileTypes = tileTypes
         self.editSurf = pygame.surface.Surface(dimensions)
@@ -46,11 +46,18 @@ class tileManager:
 
                     if y + 1 < self.dimensions[1]:
                         if currentTile.temp:
-                            prob = 0.5
+                            prob = 0.2+ (y / 1024)
                             i = random.randint(0, 100) / 100
                             if i < prob:
+
                                 self.tiles[x][y] = 0
                                 self.updatedTiles[x][y] = 1
+                            else:
+                                if y - 1 >= 0:
+                                    if self.tiles[x][y - 1] == 0:
+                                        self.tiles[x][y - 1] = currentTile.id
+                                        self.tiles[x][y] = 0
+                                        self.updatedTiles[x][y - 1] = 1
 
                         if currentTile.gravity:
                             if self.tiles[x][y + 1] in currentTile.displacingTiles:
@@ -80,36 +87,45 @@ class tileManager:
                                     self.updatedTiles[x + chosenDirection][y + 1] = 1
 
                                 elif currentTile.liquid:
+                                    foundL = False
+                                    foundR = False
                                     left = 1
                                     right = 1
                                     while x - left >= 0 and left != 0:
                                         if self.tiles[x - left][y + 1] != 3:
                                             if self.tiles[x - left][y + 1] != 0:
                                                 left = 0
+                                                foundL = False
                                                 break
                                             else:
+                                                foundL = True
                                                 break
                                         elif self.tiles[x - left][y] != 0:
                                             left = 0
+                                            foundL = False
                                             break
                                         left += 1
 
                                     while x + right < self.dimensions[0] and right != 0:
-                                        if self.tiles[x + right][y + 1] != 3:
+
+                                        if self.tiles[x + right][y + 1] != currentTile.id:
                                             if self.tiles[x + right][y + 1] != 0:
                                                 right = 0
+                                                foundR = False
                                                 break
                                             else:
+                                                foundR = True
                                                 break
                                         elif self.tiles[x + right][y] != 0:
                                             right = 0
+                                            foundR = False
                                             break
                                         right += 1
 
-                                    if left > 0 or right > 0:
-                                        if left < right and left != 0:
+                                    if (left > 0 and foundL) or (right > 0 and foundR):
+                                        if left < right and left != 0 and foundL:
                                             direction = -left
-                                        elif right < left and right != 0:
+                                        elif right < left and right != 0 and foundR:
                                             direction = right
                                         else:
                                             dlist = [-left, right]
@@ -120,6 +136,7 @@ class tileManager:
                                         self.tiles[x][y] = self.tiles[x + direction][y + 1]
                                         self.updatedTiles[x][y] = 1
                                         self.tiles[x + direction][y + 1] = currentTile.id
+                                        print(x + direction, y + 1, " | ", len(self.tiles), len(self.tiles[x]), " | ", len(self.updatedTiles), len(self.updatedTiles[x]))
                                         self.updatedTiles[x + direction][y + 1] = 1
 
     def updateSurf(self, offsetX, offsetY):
